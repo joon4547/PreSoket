34835c388f75fbdebf3680a3a2c2c456eeb08bad
hjshin <joon4547@gamil.com>
2018-03-07 11:44:49 +0900

    원속 초기화, 소켓생성 주석 추가

--- ConsoleApplication1/SoketStart.cpp
+++ ConsoleApplication1/SoketStart.cpp
@@ -11,7 +11,9 @@ int main(int argc, char * argv[])
 
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);	// "인터넷 영역을 사용하는 TCP방식의 프로토콜"
 
-	if (sock == INVALID_SOCKET)
+	if (sock == INVALID_SOCKET) //INVALID_SOCKET은 양수가 아닌 에러코드 갑을 의미한다. 
+								//왜냐하면 SOCKET 자료형이 unsinged int 양수 이기 때문.
+								//socket은 핸들 값으로 나오기 때문에 선언한 변수에 저장시켜서 이용해야 한다.
 		return 1;
 	
 	printf("성공\n");
